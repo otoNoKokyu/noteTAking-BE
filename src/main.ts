28567b803ed6f136af8556415070224ea514c444
otoNoKokyu <arko466@gmail.com>
2026-03-26 03:07:17 +0530

    SMALL FIX

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
+    app.useWebSocketAdapter(new IoAdapter(app));
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await app.listen(process.env.PORT ?? 3000);
